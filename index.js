f3596796e639af8453e038f81dc6fff10d83110a
Jaber Molla <molla.jaber@gmail.com>
2026-06-20 02:22:11 +0600

    fixed bugs

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const app = express()
 const port = 5000
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const dotenv = require('dotenv');
 dotenv.config();
+app.use(cors());
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -28,12 +32,22 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    //does actions here
+    const database = client.db(process.env.DATABASE_NAME);
+    const profilecollection = database.collection('lawyer_sp_profiles');
+    app.post('/api/legal_profiles',  async (req, res)  => {
+        const profile = req.body;
+        const result = await profilecollection.insertOne(profile);
+        res.send(result);
+      
+    })
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    //await client.close();
   }
 }
 run().catch(console.dir);
